ddd312d1d143579d2ebb8641bfbc5cf506b71a01
Arthur.c.s <130755987+arthur-canofre@users.noreply.github.com>
2024-10-23 22:58:38 -0300

    commit quase nada

--- mobile/app-musical/app/login/index.jsx
+++ mobile/app-musical/app/login/index.jsx
@@ -1,9 +1,28 @@
 import React,{useState} from "react";
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
+import { View, Text, TextInput, Pressable, StyleSheet } from 'react-native'
 import { Link } from 'expo-router'
 
 const style = StyleSheet.create({
+    container:{
+        height: '100%',
+        width: '100%',
+        backgroundColor: '#03045E',
+    },
+    inpContainer:{
 
+    },
+    input:{
+        backgroundColor: '#0096C7',
+        borderColor: '#00B4D8',
+        borderWidth: 1,
+        borderRadius: 10,
+        width: 250,
+        height: 45
+        
+    },
+    botao: {
+        
+    }
 })
 
 export default Cadastro = () => {
@@ -17,26 +36,30 @@ export default Cadastro = () => {
     const onChangeText = (value, campo) =>{
         const newCredenciais = {...credenciais, [campo]: value}
         setCredenciais(newCredenciais)
-        console.log(credenciais)
     }
     return(
-        <View>
+        <View style={style.container}>
+            <View style={style.inpContainer}>
                 <TextInput
+                    style={style.input}
                     placeholder="Email"
                     value={credenciais.email}
                     onChangeText={(value) => {onChangeText(value, "email")}}
                 />
                 <TextInput
+                    style={style.input}
                     placeholder="Senha"
                     value={credenciais.senha}
                     onChangeText={(value) => {onChangeText(value, "senha")}}
                 />
                 { isValid === false? <Text>Crenciais Invalidas</Text>: null}
-                <Button
+            </View>
+                <Pressable
+                    style={style.botao}
                     title="Confirmar"
                     onPress={() => {!credenciais.email || !credenciais.senha? setIsValid(false): [setIsValid(true),alert("tudo certo amigao")]}}
-                />
-                <Link href='/cadastro'><Text>Não tem conta?</Text></Link>
+                ><Text>Entrar</Text></Pressable>
+                <Link href='/cadastro'><Text style={style.link}>Não tenho conta</Text></Link>
         </View>
     )
 }
